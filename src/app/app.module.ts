7792bbbf17ec3c438b5d16547e6a008f372cfcaa
Leszek <les.guziak@envoyglobal.com>
2022-08-12 11:25:14 -0500

    PL course about schematics

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';import { SomePathtoCityModule } from './blue-orange/some-pathto-city/some-pathto-city.module';
+import { SomePathtoCityModule } from './blue-orange/some-pathto-city/some-pathto-city.module';import { SomePathtoCityModule } from './blue-orange/some-pathto-city/some-pathto-city.module';
+import { SomePathtoCityModule } from './blue-orange/some-pathto-city/some-pathto-city.module';import { SomePathtoCityModule } from './blue-orange/some-pathto-city/some-pathto-city.module';
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, TestThingModule, SomePathtoCityModule, SomePathtoCityModule, SomePathtoCityModule, SomePathtoCityModule, SomePathtoCityModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
